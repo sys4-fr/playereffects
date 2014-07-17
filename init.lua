5461e148e5a900e2d0ff1143e49fa91d32a28770
Wuzzy <almikes@aol.com>
2014-07-17 18:44:15 +0200

    Less log file spam

--- init.lua
+++ init.lua
@@ -225,7 +225,7 @@ function playereffects.cancel_effect(effect_id)
 		end
 		playereffects.effect_types[effect.effect_type_id].cancel(effect, player)
 		playereffects.effects[effect_id] = nil
-		minetest.log("action", "[playereffects] Effect type "..effect.effect_type_id.." cancelled from player "..effect.playername.."!")
+--		minetest.log("action", "[playereffects] Effect type "..effect.effect_type_id.." cancelled from player "..effect.playername.."!")
 	end
 end
 
